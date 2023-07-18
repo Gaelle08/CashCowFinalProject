2d78dd99061faf8722e313584dee24d896fb55f0
Gaelle <gaellee.valmir@gmail.com>
2023-07-18 13:04:26 -0400

    Initial Commit

---
+++ CashCowFinalProject/CashCowFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashCowFinalProjectApp.swift
+//  CashCowFinalProject
+//
+//  Created by Gaelle Valmir on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CashCowFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
